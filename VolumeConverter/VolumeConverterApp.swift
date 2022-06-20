6620958fd6a4716b86855a18bb48b6148c7f8d76
MMTabaka <miloszmtabaka@gmail.com>
2022-06-20 02:15:17 +0100

    Initial Commit

---
+++ VolumeConverter/VolumeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeConverterApp.swift
+//  VolumeConverter
+//
+//  Created by Milosz Tabaka on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct VolumeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
